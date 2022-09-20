b7e55c73436ac70c27f171aaf7b140a8c1fbf8e6
Guihgo <guihgo100milha@gmail.com>
2022-09-20 14:17:51 -0300

    add emvqrcode tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Web3Helper from './web3';
 
 function App() {
+
+  const web3Helper = new Web3Helper("any_private_key")
+
   return (
     <div className="App">
       <header className="App-header">
