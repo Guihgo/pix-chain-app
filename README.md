f562a5c2600618d50d563e9a1ea3fff422bcc403
Guihgo <guihgo100milha@gmail.com>
2022-10-07 02:09:40 -0300

    refactor

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ yarn start
 ### Support web3 in react
 
 [https://web3auth.io/docs/troubleshooting/webpack-issues](https://web3auth.io/docs/troubleshooting/webpack-issues)
+[https://github.com/webpack/node-libs-browser](https://github.com/webpack/node-libs-browser)
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -320,7 +320,6 @@ module.exports = function (webpackEnv) {
         // "crypto": false,
         // "assert": false,
         "buffer": require.resolve('buffer/'),
-        "Buffer": require.resolve('buffer/')
         // "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
       },
       // These are the reasonable defaults supported by the Node ecosystem.
