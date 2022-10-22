bf1c5165dc677fafa05e0ed4af68c51b098faf6d
Guihgo <guihgo100milha@gmail.com>
2022-10-22 05:28:18 -0300

    add evm-emv-web3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import WalletHelper from "./web3";
+
 import { ThemeProvider } from "styled-components";
 import { TestColor } from "./Components/teste";
 import themes from "./Global/themes";
@@ -9,9 +9,10 @@ import MobileContainer from "./Components/MobileContainer";
 import Header from "./Components/Header";
 import Home from "./Screens/Home";
 import Router from "./routes";
-import { ECurrencySymbol } from "./web3/types";
-import { EthereumCode } from "emvqrcode-tools";
 
+import { EthereumCode } from "emvqrcode-tools";
+import { WalletHelper } from "evm-emv-web3";
+import { BinanceSmartChain_Testnet, ECurrencySymbol } from "evm-emv-web3/networks/BinanceSmartChain";
 <link
   href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
   rel="stylesheet"
@@ -26,7 +27,7 @@ function App() {
     /* load wallet */
     try {
       console.log("Opening wallet using PrivateKey: ", privateKey);
-      setWalletHelper(new WalletHelper(privateKey));
+      setWalletHelper(new WalletHelper(BinanceSmartChain_Testnet, privateKey));
       console.log("Wallet loaded with success!");
     } catch (e) {
       console.error("Invalid private key");
@@ -46,17 +47,17 @@ function App() {
     if (!walletHelper) {
       loadWallet()
     } else {
-      // /* Get Address */
-      // console.log("Address: ", walletHelper.getAddress(), walletHelper.shortAddress())
+      /* Get Address */
+      console.log("Address: ", walletHelper.getAddress(), walletHelper.shortAddress())
 
-      // /* Get Balance: Base coin (BNB (bsc-mainnet) | ETH (eth-mainnet) | TBNB (bsc-testnet) ) */
+      /* Get Balance: Base coin (BNB (bsc-mainnet) | ETH (eth-mainnet) | TBNB (bsc-testnet) ) */
       // walletHelper!.getBalance().then((balance) => {
-      //   console.log(`Balance: ${balance.amount} ${balance.symbol}`)
+      //   console.log(`Balance: ${balance} ${walletHelper.getBaseCurrency().symbol}`)
       // })
 
       // /* Get Balance: Token (USDT | SHIB | PixCoin) */
-      // walletHelper!.getBalance(ECurrencySymbol.PIX_COIN).then((balance) => {
-      //   console.log(`Balance: ${balance.amount} ${balance.symbol}`)
+      // walletHelper!.getBalance(walletHelper.getCurrency(ECurrencySymbol.PIX_COIN)).then((balance) => {
+      //   console.log(`Balance: ${balance} ${walletHelper.getCurrency(ECurrencySymbol.PIX_COIN).symbol}`)
       // })
 
       // /* Create Invoice: Base coin */
@@ -66,23 +67,23 @@ function App() {
       // })
 
       // /* Create Invoice: Token */
-      // walletHelper!.createInvoice("0xc7BE55e895Ee523EA67Ac52cDd55f927BCF6B754", "1.17", ECurrencySymbol.PIX_COIN)
+      // walletHelper!.createInvoice("0xc7BE55e895Ee523EA67Ac52cDd55f927BCF6B754", "1.17", walletHelper.getCurrency(ECurrencySymbol.PIX_COIN))
       // .then(ethCode => {
-      //   console.log(`Ethreum Transaction Code: `, ethCode, EthereumCode.parse(ethCode))
+      //   console.log(`Ethreum Transaction Code: `, ethCode, EthereumCode.parse(ethCode, walletHelper.getCurrency(ECurrencySymbol.PIX_COIN).token?.abi))
       // })
 
       /* Sign Invoice: Base coin */
-      // walletHelper!.signInvoice("00020126770023ETHEREUM.BLOCKCHAIN.PIX0146https://data-seed-prebsc-1-s3.binance.org:8545A014901030x202420xc7BE55e895Ee523EA67Ac52cDd55f927BCF6B75403420xAB518cD35e0Cc361Fe7687C3CF6b81147AA62d7404029705160x2386f26fc1000006052100007110x37e11d60063042431").then(r=>{
+      // walletHelper!.signInvoice("00020126770023ETHEREUM.BLOCKCHAIN.PIX0146https://data-seed-prebsc-1-s3.binance.org:8545A014901030xa02420xc7BE55e895Ee523EA67Ac52cDd55f927BCF6B75403420xAB518cD35e0Cc361Fe7687C3CF6b81147AA62d7404029705160x2386f26fc1000006052100007110x37e11d60063043841").then(r=>{
       //   console.log(r)
       // })
 
       /* Sign Invoice: Token */
-      // walletHelper!.signInvoice("00020126770023ETHEREUM.BLOCKCHAIN.PIX0146https://data-seed-prebsc-1-s3.binance.org:8545A027901030x302420xc7BE55e895Ee523EA67Ac52cDd55f927BCF6B75403420xEE5A767BDB73fafD4bB50317A8A78901a81547f404029705030x006053688107110x37e11d600A81380xa9059cbb000000000000000000000000ab518cd35e0cc361fe7687c3cf6b81147aa62d74000000000000000000000000000000000000000000000000103caccd1335000063047733 ").then(r=>{
+      // walletHelper!.signInvoice("00020126770023ETHEREUM.BLOCKCHAIN.PIX0146https://data-seed-prebsc-1-s3.binance.org:8545A027901030xb02420xc7BE55e895Ee523EA67Ac52cDd55f927BCF6B75403420xEE5A767BDB73fafD4bB50317A8A78901a81547f404029705030x006053688107110x37e11d600A81380xa9059cbb000000000000000000000000ab518cd35e0cc361fe7687c3cf6b81147aa62d74000000000000000000000000000000000000000000000000103caccd1335000063044941").then(r=>{
       //   console.log(r)
       // })
 
       /* Send Signed Transaction */
-      // walletHelper!.sendSignedTransaction("0xf8aa0385037e11d60082901194ee5a767bdb73fafd4bb50317a8a78901a81547f480b844a9059cbb000000000000000000000000ab518cd35e0cc361fe7687c3cf6b81147aa62d74000000000000000000000000000000000000000000000000103caccd1335000081e6a097ccab46b482c99abb320ad24d87a7665df6d818f7a13bf41770b1d3a0575befa04769c71386df37133af7788695ce5af3fcfcac4ae08549f5915823c8b3b76141").then(result=>{
+      // walletHelper!.sendSignedTransaction("0xf8aa0b85037e11d60082901194ee5a767bdb73fafd4bb50317a8a78901a81547f480b844a9059cbb000000000000000000000000ab518cd35e0cc361fe7687c3cf6b81147aa62d74000000000000000000000000000000000000000000000000103caccd1335000081e6a04c3cf7b1c6a65aa3cbb66b0e86c3d715565e85a047fc764269b11ddf10873916a062b642a1bf15ad990010be14608c8cd959442e73a4e7b100c0b801cec7bff938").then(result=>{
       //   console.log(result)
       // })
     }
--- src/web3/utils.ts
+++
@@ -1,3 +0,0 @@
-export function shortLongString(str: string, start = 6, end = -4) {
-    return `${str.slice(0, start)}...${str.slice(end)}`
-}
