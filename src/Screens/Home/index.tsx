6e9e50a58974452767219c7f86c28fb806cc5c75
MariaClara7 <mcadelmonico@gmail.com>
2022-11-02 19:32:16 -0300

    qr-code

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -4,6 +4,7 @@ import Input from "../../Components/Input";
 import KeyText from "../../Components/KeyText";
 import PXCArea from "../../Components/PXCArea";
 import { Container, KeyContainer, Title } from "./styles";
+import {QRCodeSVG} from 'qrcode.react';
 
 function Home() {
   const [keyValue, setKeyValue] = useState("5KjEHU53Xk5Ga...");
@@ -21,6 +22,16 @@ function Home() {
       </KeyContainer>
       <div style={{ marginTop: "2.3rem" }}>
         <PXCArea title="Carteira A" value="100,00" />
+      </div>
+      <div>
+        <QRCodeSVG
+          value={"https://google.com"}
+          size={128}
+          bgColor={"#ffffff"}
+          fgColor={"#000000"}
+          level={"L"}
+          includeMargin={false}
+        />
       </div>
     </>
   );
