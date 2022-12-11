25560eff770668f80c3be2d3a101e6ca3e4d507b
Guihgo <guihgo100milha@gmail.com>
2022-12-10 22:12:48 -0300

    [fix] react render

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
